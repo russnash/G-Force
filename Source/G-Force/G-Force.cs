9aac07f40541ae0964cf79c61e8d10aff89c9167
Russ Nash <russnash37@yahoo.com>
2014-12-30 17:43:26 -0500

    Initial commit

---
+++ Source/G-Force/G-Force.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace G_Force
+{
+    [KSPAddon(KSPAddon.Startup.Flight, true)]
+    public class G_Force : MonoBehaviour
+    {
+        double currentG;
+        float initialSharpness;
+
+        protected void Start()
+        {
+            DontDestroyOnLoad(this);
+            initialSharpness = FlightCamera.fetch.sharpness;
+            Debug.Log("Initial Camera Sharpness: " + initialSharpness.ToString());
+        }
+
+        public void Update()
+        {
+            currentG = FlightGlobals.ActiveVessel.geeForce;
+            if (currentG >= 2f)
+            {
+                //UnityEngine.Camera.current.
+            }
+        }
+    }
+}
