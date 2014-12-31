a055bca4a60dbdcc734f2ea0464a9091fc6c7321
Russ Nash <russnash37@yahoo.com>
2014-12-31 15:12:16 -0500

    Experiments with visual effects

--- Source/G-Force/G-Force.cs
+++ Source/G-Force/G-Force.cs
@@ -10,22 +10,42 @@ namespace G_Force
     public class G_Force : MonoBehaviour
     {
         double currentG;
-        float initialSharpness;
+        float initialFOV;
+        Texture2D redout = new Texture2D(Screen.width, Screen.height);
+        Texture2D blackout = new Texture2D(Screen.width, Screen.height);
 
         protected void Start()
         {
             DontDestroyOnLoad(this);
-            initialSharpness = FlightCamera.fetch.sharpness;
-            Debug.Log("Initial Camera Sharpness: " + initialSharpness.ToString());
+            initialFOV = FlightCamera.fetch.fovDefault;
+            RenderingManager.AddToPostDrawQueue(3, new Callback(drawEffect));
+        }
+
+        private void drawEffect()
+        {
+            float factor = (float)currentG / 1.5f;
+            ScreenMessages.PostScreenMessage(currentG.ToString() + " / " + factor.ToString());
+            guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+            
+
+            if (factor > 1f)
+            {
+                //FlightCamera.fetch.SetFoV(initialFOV / factor);
+                guiTexture.color = new Color(0.0f, 0.0f, 0.0f, factor / 10);
+                guiTexture.enabled = true;
+            }
+            else
+            {
+                //FlightCamera.fetch.SetFoV(initialFOV);
+                guiTexture.color = Color.clear;
+                guiTexture.enabled = false;
+            }
+            
         }
 
         public void Update()
         {
             currentG = FlightGlobals.ActiveVessel.geeForce;
-            if (currentG >= 2f)
-            {
-                //UnityEngine.Camera.current.
-            }
         }
     }
 }
