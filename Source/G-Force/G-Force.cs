c7812fac5aba153f5e8d9c268a18b00fca22908d
Russ Nash <russnash37@yahoo.com>
2015-01-29 20:17:32 -0500

    Version 0.0.4-alpha WIP release

--- Source/G-Force/G-Force.cs
+++ Source/G-Force/G-Force.cs
@@ -25,6 +25,8 @@ namespace G_Force
         bool isPositiveG = true;
         CelestialBody mainBody;
         Vector3d mainBodyCOM;
+        double distOrbCOM;
+        double distOrbCOM2AccellPos;
 
         protected void Start()
         {
@@ -37,7 +39,7 @@ namespace G_Force
             RenderingManager.AddToPostDrawQueue(3, new Callback(drawGEffects));
 
             // Add another rendering queue hook for the GUI
-            RenderingManager.AddToPostDrawQueue(4, new Callback(drawGUI));
+            //RenderingManager.AddToPostDrawQueue(4, new Callback(drawGUI));
         }
 
         void drawGEffects()
@@ -50,37 +52,59 @@ namespace G_Force
             else
             {
                 colorOut = Color.red;
-                colorOut.a = (float)currentG / 2;
+                colorOut.a = (float)currentG / 10;
             }
 
-            //ScreenMessages.PostScreenMessage(cumulativeG.ToString() + " / " + colorOut.a.ToString());
+            //ScreenMessages.PostScreenMessage(cumulativeG.ToString());
 
-            GUI.color = colorOut;
-            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), whiteTexture);
-            GUI.color = Color.white;
+            if (FlightGlobals.ActiveVessel.horizontalSrfSpeed > 50 & FlightGlobals.ActiveVessel.situation != Vessel.Situations.LANDED) // Dirty hack to stop the 'drunk prograde marker' causing weird G effects when stopped
+            {
+                GUI.color = colorOut;
+                GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), whiteTexture);
+                GUI.color = Color.white;
+            }
         }
 
         void drawGUI()
         {
-
+            // Good in 'ere, init!
         }
 
         public void Update()
         {
-            // Retrieve the orbited celestial body.
+            // Retrieve the center of mass of the orbited celestial body.
             mainBody = FlightGlobals.ActiveVessel.mainBody;
-
-            // Retrieve the position of the main orbited bodies COM in world coords.
             mainBodyCOM = mainBody.position;
 
             // Retrieve the current G forces.
             currentG = FlightGlobals.ActiveVessel.geeForce;
 
+            // Calculate the distance from the orbited bodies COM to the current vessels COM
+            distOrbCOM = Vector3d.Distance(mainBodyCOM, FlightGlobals.ActiveVessel.GetWorldPos3D());
+
+            // Calculate the distance from the orbited bodies COM to the current vessels COM with the accelleration vector applied
+            distOrbCOM2AccellPos = Vector3d.Distance(mainBodyCOM, FlightGlobals.ActiveVessel.GetWorldPos3D() + FlightGlobals.ActiveVessel.acceleration);
+
+            // If the vessels position plus its acceleration vector is further from the orbited bodies COM than the vessel itself is,
+            // then we are experiencing positive G's, otherwise negative.
+            if (distOrbCOM2AccellPos > distOrbCOM)
+            {
+                isPositiveG = true;
+            }
+            else
+            {
+                isPositiveG = false;
+            }
+
+            // Now, the above calculations assume that we are right-side up (not flying upside down!), so, if we are inverted we need
+            // to invert the G type.
+
+
             // If we're experiencing 4G's plus, grow the cumulativeG with relation to the current G and ensure
             // we don't wrap to a negative value.
             if (currentG >= 4)
             {
-                factor = currentG * currentG;
+                factor = currentG * (currentG / 4);
                 if (32767 - cumulativeG <= factor)
                 {
                     cumulativeG = 32767;
